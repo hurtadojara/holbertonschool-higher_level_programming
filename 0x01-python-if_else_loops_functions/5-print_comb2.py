6031e6aacb4f30c9e61b5da3d41abebbcc43751f
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-09-01 21:09:31 -0500

    pep8

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-i = 0
-for i in range(100):
-    if i == 99:
-        print("{0}".format(i))
-    print("{:02d}, ".format(i), end="")
+for i in range(99):
+    print("{:02d}".format(i), end=', ')
+print(i + 1)
