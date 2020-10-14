078c031ebfc6fb5b4d828b6566c79115d9ab9f39
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-10-13 21:56:46 -0500

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,59 +30,47 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """width getter for class"""
-        return self.width
+        """Getter for width"""
+        return self.__width
 
     @width.setter
-    def width(self, width):
-        """setter width"""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = width
+    def width(self, value):
+        """setter for width"""
+        self.validator("width", value)
+        self.__width = value
 
     @property
     def height(self):
-        """getter height"""
+        """Getter for height"""
         return self.__height
 
     @height.setter
-    def height(self, height):
-        """setter of height"""
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = height
+    def height(self, value):
+        """setter for height"""
+        self.validator("height", value)
+        self.__height = value
 
     @property
-    def x(self, x):
-        """ getter of X """
+    def x(self):
+        """Getter for x"""
         return self.__x
 
     @x.setter
-    def x(self, x):
-        """ setter of X """
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = x
+    def x(self, value):
+        """setter for x"""
+        self.validator("x", value)
+        self.__x = value
 
     @property
     def y(self):
-        """ getter of Y """
+        """Getter for y"""
         return self.__y
 
     @y.setter
-    def y(self, y):
-        """ setter of Y """
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = y
+    def y(self, value):
+        """setter for y"""
+        self.validator("y", value)
+        self.__y = value
 
     def area(self):
         """function to return the area"""
