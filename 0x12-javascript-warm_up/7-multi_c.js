14d7f08f50f223a8740c752899a9cde069d6868b
hurtadojara <andreshurtadojaramillo@gmail.com>
2021-02-02 21:47:49 -0500

    FULL JS NODE IS GREAT

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2])) === false) {
-  const i = process.argv.length;
-  for (const iterator in process.argv) {
-    if (i >= iterator) {
-      console.log('C is fun');
-    }
+  const i = parseInt(process.argv[2]);
+  let j = 0;
+  while (j < i) {
+    console.log('C is fun');
+    j++;
   }
 } else {
   console.log('Missing number of occurrences');
